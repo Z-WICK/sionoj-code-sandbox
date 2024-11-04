08fd92fa98a04c40bddbeac4dcfcb315cb16e16c
Z-WICK <2289431216@qq.com>
2024-11-04 20:47:19 +0800

    feat: 调用方与服务提供方之间约定一个字符串 保证API安全性

--- src/main/java/com/sion/sionojcodesandbox/controller/MainController.java
+++ src/main/java/com/sion/sionojcodesandbox/controller/MainController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 
 /**
  * @Author : wick
@@ -16,6 +18,11 @@ import javax.annotation.Resource;
  */
 @RestController("/")
 public class MainController {
+
+        // 定义鉴权请求头和密钥
+    private static final String AUTH_REQUEST_HEADER = "auth";
+
+    private static final String AUTH_REQUEST_SECRET = "secretKey";
 
     @Resource
     private JavaNativeCodeSandbox javaNativeCodeSandbox;
@@ -28,7 +35,15 @@ public class MainController {
 
 
     @PostMapping("/executeCode")
-    ExecuteCodeResponse executeCode (@RequestBody ExecuteCodeRequest executeCodeRequest){
+    ExecuteCodeResponse executeCode (@RequestBody ExecuteCodeRequest executeCodeRequest,
+                                     HttpServletRequest request,
+                                     HttpServletResponse response){
+        // 基本的认证
+        String authHeader = request.getHeader(AUTH_REQUEST_HEADER);
+        if (!AUTH_REQUEST_SECRET.equals(authHeader)) {
+            response.setStatus(403);
+            return null;
+        }
         if(executeCodeRequest == null){
             throw new RuntimeException("请求参数为空");
         }
